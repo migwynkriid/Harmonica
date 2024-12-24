46ca583226df2d8ba463505c25f2566ff50716ae
migwynkriid <sebastijanaqw@gmail.com>
2024-12-24 15:25:04 +0100

    wording

--- scripts/load_commands.py
+++ scripts/load_commands.py
@@ -2,7 +2,6 @@ import os
 
 async def load_commands(bot):
     commands_dir = 'commands'
-    print('-' * 40)
     for filename in os.listdir(commands_dir):
         if filename.endswith('.py') and not filename.startswith('_'):
             extension_name = f'{commands_dir}.{filename[:-3]}'
