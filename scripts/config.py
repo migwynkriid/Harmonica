23536284dc736b4ab9a0bcb710b0b162f6409069
migwynkriid <sebastijanaqw@gmail.com>
2025-01-05 18:20:38 +0100

    ffmpeg update

--- scripts/config.py
+++ scripts/config.py
@@ -92,5 +92,5 @@ YTDL_OPTIONS = {
 
 FFMPEG_OPTIONS = {
     'executable': FFMPEG_PATH,
-    'options': '-loglevel warning -vn -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -threads 4',
+    'options': '-loglevel warning -vn -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -threads 4 -af aresample=async=1 -buffer_size 64k',
 }
